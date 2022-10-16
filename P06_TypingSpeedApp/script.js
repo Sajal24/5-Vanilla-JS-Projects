da99d06efbccdd7ce0d95911772face94e1e3166
Sajal Sharma <sajalsharma2408@gmail.com>
2022-10-16 20:58:51 +0530

    updates in P06

---
+++ P06_TypingSpeedApp/script.js
@@ -0,0 +1,7 @@
+// 1. Display 5-6 chunks of sentances in the givenText area
+// 2. Every time I click the reset button the text changes automatically
+// 3. Wen i start writing, the clock starts
+// 4. keep checking each alphabet of writingArea to givenTextArea - loop
+// 5. if any alphabet doesn't match - red bg and a error sound
+// 6. nothing and green bg if all going good
+// 7. when all the letters matches, stop the watch and play a good sound
