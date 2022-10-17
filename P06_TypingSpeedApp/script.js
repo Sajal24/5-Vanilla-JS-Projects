0a993295b8d7788ade877c996d809e7789acdb4f
Sajal Sharma <sajalsharma2408@gmail.com>
2022-10-17 23:15:04 +0530

    text area functionality added

--- P06_TypingSpeedApp/script.js
+++ P06_TypingSpeedApp/script.js
@@ -8,22 +8,45 @@
 
 //selecting all elements
 const givenTextArea = document.querySelector(".givenText");
-const areaToWrite = document.querySelector(".areaToWrite");
+const borderChange = document.getElementById("areaToWrite");
+const areaToWrite = document.querySelector("#inputArea");
 const timeCount = document.querySelector(".allotedTime");
 const resetButton = document.querySelector(".btn");
+let i = 0;
 
 //making an array containing chunks of sentances
 const sentances = [
-  "insomnia sharpens your math skills because you spend all night calculating how much sleep you will get if you are able to fall asleep right now",
   "its only after we have lost everything that we are free to do anything by tyler durden from fight club",
+  "insomnia sharpens your math skills because you spend all night calculating how much sleep you will get if you are able to fall asleep right now",
   "i could die right now clem i am just happy i have never felt that before i am just exactly where i want to be",
   "sometimes it makes me sad though with andy being gone i have to remind myself that some birds aren not meant to be caged their feathers are just too bright and when they fly away the part of you that knows it was a sin to lock them up does rejoice",
   "wait a minute i think i left my conscience on your front doorstep oh oh wait a minute i think i left my consciousness in the sixth dimension but i am here right now right now",
 ];
 
+let randomSentance =
+  "its only after we have lost everything that we are free to do anything by tyler durden from fight club";
+
+//reset button functionality - to change the sentances in a random order
 resetButton.addEventListener("click", randomSentanceGenerator);
 
 function randomSentanceGenerator() {
-  let random = sentances[Math.floor(Math.random() * sentances.length)];
-  givenTextArea.innerHTML = random;
+  randomSentance = sentances[Math.floor(Math.random() * sentances.length)];
+  givenTextArea.innerHTML = randomSentance;
 }
+
+// start checking the writing text letter by letter and when wrong letter - borders: red and if going right - borders: blue; and at last when all sentance.letter.length is equal to written sentences.letter.length with every letter correct -> border: dark green;
+
+areaToWrite.addEventListener("keypress", (e) => {
+  if (i < randomSentance.length) {
+    if (e.key === randomSentance[i]) {
+      console.log("y");
+      if (i == randomSentance.length - 1) {
+        borderChange.style.borderColor = "green";
+      } else borderChange.style.borderColor = "blue";
+      i++;
+    } else {
+      console.log("no");
+      borderChange.style.borderColor = "red";
+    }
+  }
+});
